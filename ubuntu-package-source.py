83bec9335de75826a66c7509de87d37c6de331de
Manh Thiep <manhthiep@gmail.com>
2015-02-02 18:55:36 +0700

    Add options for ubuntu-package-source.py

--- ubuntu-package-source.py
+++ ubuntu-package-source.py
@@ -4,6 +4,7 @@ import sys
 import signal
 import os
 import errno
+import getopt
 
 from re import match, split
 from subprocess import check_output
@@ -13,6 +14,10 @@ from debian.deb822 import Deb822
 package_nodes = {}
 package_info = {}
 package_cache = []
+
+DEBUG = False
+VERBOSE = False
+NO_DEPENDS = False
 
 class PackageNode:
 
@@ -86,16 +91,32 @@ def get_node_for_package(pkg_name):
     if pkg_name not in package_nodes.keys():
         pkg_info = get_package_info(pkg_name)
         if pkg_info:
+            if VERBOSE:
+                print "Get package info: " + pkg_name
             pkg_node = PackageNode(pkg_name)
             package_nodes[pkg_name] = pkg_node
             # add node properties:
-            pkg_node.source = pkg_info.get('Source', '')
             pkg_node.version = pkg_info.get('Version', '')
-            depends = pkg_info.get('Depends', '')
-            depends = get_sanitised_depends_list(depends)
-            for dep in depends:
-                dep_node = get_node_for_package(dep)
-                pkg_node.addDepend(dep, dep_node)
+            source = pkg_info.get('Source', '')
+            if source == '':
+                # source_pkg_name == pkg_name
+                pkg_node.source = pkg_name
+                if VERBOSE:
+                    print "  Getting source package: '%s' for '%s'" % (pkg_name, pkg_name)
+            else:
+                # source_pkg_name != pkg_name
+                pkg_node.source = match('(\S*)', source.strip()).groups()[0]
+                if VERBOSE:
+                    print "  Getting source package: '%s' instead of '%s'" % (pkg_node.source, pkg_name)
+            if not NO_DEPENDS:
+                depends = pkg_info.get('Depends', '')
+                depends = get_sanitised_depends_list(depends)
+                depends = sorted(depends)
+                if DEBUG:
+                    print "  Depends: " + ', '.join(depends)
+                for dep in depends:
+                    dep_node = get_node_for_package(dep)
+                    pkg_node.addDepend(dep, dep_node)
             return pkg_node
         else:
             return None
@@ -122,27 +143,20 @@ def get_depends(pkg_name):
     return depends_list
 
 def get_source_for_package(pkg_name):
-    """Get source package for a package"""
+    """Get source for a package"""
 
     source_pkg_name = ''
     pkg_node = get_node_for_package(pkg_name)
     if pkg_node:
-        if pkg_node.source == '':
-            # source_pkg_name == pkg_name
-            source_pkg_name = pkg_name
-            print "Getting source package: '%s' for '%s'" % (pkg_name, pkg_name)
-        else:
-            # source_pkg_name != pkg_name
-            # source_pkg_name = pkg_node.source
-            source_pkg_name = match('(\S*)', pkg_node.source.strip()).groups()[0]
-            print "Getting source package: '%s' instead of '%s'" % (source_pkg_name, pkg_name)
+        source_pkg_name = pkg_node.source
     else:
-        print "Invalid package name: '%s'" % pkg_name
+        print "Skip invalid package name: '%s'" % pkg_name
         source_pkg_name = ''
 
     return source_pkg_name
 
 def get_source_for_package_and_depends(pkg_name):
+    """Get sources for a package and its depends"""
 
     source_pkgs = []
     
@@ -158,18 +172,83 @@ def get_source_for_package_and_depends(pkg_name):
 
     return source_pkgs
 
+def ask_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
 def signal_handler(signal, frame):
     sys.exit(0)
+
+def usage(script_name):
+    print "Usage: %s [OPTIONS] PACKAGE_NAMES" % script_name
+    print "Options:"
+    print "  --no-depends"
+    print "  --debug"
+    print "  --verbose"
+    print ""
 
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, signal_handler)
 
     if len(sys.argv) == 1:
-        print "Missing package names"
+        print "Missing arguments"
+        usage(sys.argv[0])
         sys.exit(1)
 
-    packages = sys.argv[1:]
+    try:
+        opts, packages = getopt.getopt(sys.argv[1:], '', ['no-depends', 'debug', 'verbose'])
+    except getopt.GetoptError as err:
+        print str(err) # will print something like "option -a not recognized"
+        usage(sys.argv[0])
+        sys.exit(2)
+
+    if len(packages) == 0:
+        print "Missing package names"
+        usage(sys.argv[0])
+        sys.exit(3)
+
+    DEBUG = False
+    VERBOSE = False
+    NO_DEPENDS = False
+    for o, a in opts:
+        if o == "--no-depends":
+            NO_DEPENDS = True
+        elif o == "--debug":
+            DEBUG = True
+            VERBOSE = True
+        elif o == "--verbose":
+            VERBOSE = True
+
+    # packages = sys.argv[1:]
     # packages = [ 'ubuntu-desktop' ]
     # print "Packages: "  + ', '.join(packages)
 
@@ -177,14 +256,28 @@ if __name__ == '__main__':
     for pkg_name in packages:
         print "Package: " + pkg_name
 
-        source_pkgs = get_source_for_package_and_depends(pkg_name)
-        for source in sorted(source_pkgs):
+        if NO_DEPENDS:
+            source_pkgs = [get_source_for_package(pkg_name)]
+        else:
+            source_pkgs = get_source_for_package_and_depends(pkg_name)
+        if len(source_pkgs) > 0:
             print ""
-            print "Downloading " + source
-            result = 0
-            result = download_source_package(source)
-            if result != 0:
-                sys.exit(result)
+            print "Packages to download (%d packages):" % len(source_pkgs)
+            print ', '.join(source_pkgs)
+            print ""
+            answer = ask_yes_no("Continue to download ?")
+            if answer:
+                count = 0
+                for source in sorted(source_pkgs):
+                    count = count + 1
+                    print ""
+                    print "Downloading %s (%d / %d)" % (source, count, len(source_pkgs))
+                    result = 0
+                    result = download_source_package(source)
+                    if result != 0:
+                        sys.exit(result)
+            else:
+                print "Skip downloading source(s) for package '%s'" % pkg_name
 
         n += 1
         if n % 10 == 0:
